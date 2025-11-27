722af729271c3ba13a61a142296936e3d8e2ae1a
Gilocah28 <gberxx@gmail.com>
2025-11-28 02:37:29 +0800

    Age Calculator done

--- Age Calculator/app.js
+++ Age Calculator/app.js
@@ -2,4 +2,20 @@ const dateRange = document.getElementById("date-range");
 const ageCalBtn = document.getElementById("calculate-age");
 const spanResult = document.getElementById("result");
 
+// console.log(now);
 
+ageCalBtn.addEventListener("click", ageCalculator);
+
+function ageCalculator() {
+  const birthdate = new Date(dateRange.value);
+  const dateNow = new Date();
+  let ageNow = dateNow.getFullYear() - birthdate.getFullYear();
+  const month = dateNow.getMonth() - birthdate.getMonth();
+
+  if (month < 0 || (month === 0 && dateNow.getDate() < birth.getDate())) {
+    ageNow--;
+  }
+  spanResult.innerHTML = `Your age is ${ageNow} ${
+    ageNow > 1 ? "years" : "year"
+  } old`;
+}
